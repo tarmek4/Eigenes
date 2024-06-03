4410e7419d993d827d9f789b8cd3ba57e96c3835
MichaelZ <tarmek4@gmail.com>
2024-06-03 18:38:00 +0200

    Gehts noch?

---
+++ UIOHUPIBHPIFHPUIHFH/TEST_OB_GIT_GEHT.java
@@ -0,0 +1,10 @@
+package UIOHUPIBHPIFHPUIHFH;
+
+public class TEST_OB_GIT_GEHT {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("TEst");
+	}
+
+}
